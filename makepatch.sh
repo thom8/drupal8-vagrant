38c04434965863012652d55b0160c78d9d84db10
Thom Toogood <thomtoogood@gmail.com>
2015-06-06 12:37:58 +1000

    Added makepatch.sh

---
+++ makepatch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# https://www.drupal.org/node/707484
+
+echo "Enter project name: "
+read project_name
+echo "Enter short description: "
+read short_description
+echo "Enter issue number: "
+read issue_number
+echo "Enter comment number: "
+read comment_number
+
+patchName="$(echo "$project_name-$short_description-$issue_number-$comment_number.patch" | tr ' ' "_")"
+
+cd docroot
+rm -rf $patchName
+git fetch
+git diff 8.0.x > $patchName
+mv $patchName "../$patchName"
+
+echo "Patch file created: $patchName"
