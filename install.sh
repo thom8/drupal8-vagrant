585b559337ba2d5a718709985ddd9b9f595d162f
Thom Toogood <thom.toogood@eurekareport.com.au>
2015-06-09 16:51:27 +1000

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git clone https://github.com/thom8/drupal8-vagrant.git
+cd drupal8-vagrant
+git submodule init
+git submodule update
+git submodule foreach git pull origin 8.0.x
+vagrant up
