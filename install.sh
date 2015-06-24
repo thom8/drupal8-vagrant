759c4746a25926a38d636089fdb2989238c955cd
Thom Toogood <thomtoogood@gmail.com>
2015-06-24 21:49:26 +1000

    Added vagrant-hostupdater install command.

--- install.sh
+++ install.sh
@@ -3,4 +3,7 @@
 git clone --recursive https://github.com/thom8/drupal8-vagrant.git
 cd drupal8-vagrant
 git submodule foreach git checkout 8.0.x
+if !(vagrant plugin list | grep -q vagrant-hostsupdater); then
+  vagrant plugin install vagrant-hostupdater
+fi
 vagrant up
