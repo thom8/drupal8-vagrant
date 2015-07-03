a19b6e18ea34e40e7997b7e961c46fc2b14c0180
Thom Toogood <thomtoogood@gmail.com>
2015-07-03 23:40:40 +1000

    Remove state flags with sudo.

--- reset.sh
+++ reset.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-vagrant ssh -c "rm -rf ~/vagrantup && rm -rf ~/siteinstalled"2&>/dev/null
+vagrant ssh -c "sudo rm -rf ~/vagrantup && sudo rm -rf ~/siteinstalled"2&>/dev/null
 vagrant provision
